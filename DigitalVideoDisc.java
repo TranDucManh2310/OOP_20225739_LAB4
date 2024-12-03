a5155f2472b9e96a28abbc142f69c7f5526a374c
TranDucManh2310 <tranducmanh23102004@gmail.com>
2024-12-03 17:00:41 +0700

    them cac class moi

---
+++ DigitalVideoDisc.java
@@ -0,0 +1,5 @@
+public class DigitalVideoDisc extends Disc {
+    public DigitalVideoDisc(int id, String title, String category, float cost, int length, String director) {
+        super(id, title, category, cost, length, director);
+    }
+}
